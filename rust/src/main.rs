87d3b12ced1f7b9a02a7352713584bc7ea0c0740
Tora0615 <yyang0x615@gmail.com>
2024-09-05 14:28:42 +0800

    refactor : change file naming rules

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,9 +1,9 @@
-mod two_sum;
+mod q_0001_two_sum;
 
 fn main() {
     let nums = vec![2, 7, 11, 15];
     let target = 9;
-    let result = two_sum::Solution::two_sum(nums, target);
+    let result = q_0001_two_sum::Solution::two_sum(nums, target);
     println!("{:?}", result); // Output should be [0, 1]
 }
 
